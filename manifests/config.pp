3e7ef4061576e987b43463cc3e04d1fea37f8100
Dieter De Meyer <dieterdemeyer@gmail.com>
2015-02-26 16:07:08 +0100

    Added service + sysconfig scripts

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,4 +1,17 @@
 class wildfly::config(
+  $version=undef
 ){
+
+  validate_re($version, '^[~+._0-9a-zA-Z:-]+$')
+
+  $wildfly_full_version = regsubst($version, '^(\d+\.\d+\.\d+).*','\1')
+  $wildfly_major_version = regsubst($version, '^(\d+\.\d+).*','\1')
+  $package_version = regsubst($wildfly_major_version, '\.', '', 'G')
+
+  file { "/etc/sysconfig/wildfly${package_version}":
+    ensure  => file,
+    mode    => '0640',
+    content => template("${module_name}/etc/sysconfig/wildfly.erb")
+  }
 
 }
