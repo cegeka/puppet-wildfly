0f61cb48daee262eb8aba52c18c7504aa505f3ec
Thomas Meeus <thomas@sector7g.be>
2015-07-30 08:42:17 +0200

    [Ventouris] make shutdown-wait param configurable

--- manifests/config.pp
+++ manifests/config.pp
@@ -13,6 +13,7 @@ class wildfly::config(
   $jboss_user = 'wildfly',
   $jboss_group = 'wildfly',
   $jboss_data_dir = '/opt/wildfly',
+  $jboss_shutdown_wait = '60',
   $jboss_log_dir = undef,
   $users_mgmt = [],
   $newrelic_enabled = false,
